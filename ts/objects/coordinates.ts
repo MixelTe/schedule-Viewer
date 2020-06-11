d505c9f6ea2d6304202c27a04926af2329f020f6
Mike <63655075+MixelTe@users.noreply.github.com>
2020-06-11 13:15:51 +0300

    scale

--- ts/objects/coordinates.ts
+++ ts/objects/coordinates.ts
@@ -6,9 +6,16 @@ export class Coordinates
     private y: number;
 
     private axis = { x: 0, y: 0, width: 0, height: 0, svgEl: <SVGPolylineElement>{}, color: "black", sWidth: 5 };
+    private scale = {
+        hours: { els: <any>[], color: "black", width: 2, height: 25, fontSize: 20 },
+        minutes: { els: <any>[], color: "black", width: 0.5, height: 20, fontSize: 12 },
+        seconds: { els: <any>[], color: "black", width: 2, height: 15, fontSize: 10 },
+    }
+    private oneHour: number;
 
-    constructor(body: SVGElement, bodyPrm: Rect, map: Map<EventTarget, any>)
+    constructor(body: SVGElement, bodyPrm: Rect, oneHour: number)
     {
+        this.oneHour = oneHour;
         this.x = bodyPrm.x;
         this.y = bodyPrm.y;
         this.width = bodyPrm.width;
@@ -20,27 +27,98 @@ export class Coordinates
         this.axis.height = this.height;
 
         this.axis.svgEl = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
-        this.axis.svgEl.setAttribute("points", this.createPoints(
-            [
-                this.axis.x, this.axis.height + this.axis.y,
-                this.axis.x, this.axis.y,
-                this.axis.width + this.axis.x, this.axis.y
-            ]));
-
+        this.axis.svgEl.setAttribute("points", this.createAxisPoints());
         this.axis.svgEl.setAttribute("stroke", `${this.axis.color}`);
         this.axis.svgEl.setAttribute("fill", `transparent`);
         this.axis.svgEl.setAttribute("stroke-width", `${this.axis.sWidth}`);
-        map.set(this.axis.svgEl, this.axis);
         body.appendChild(this.axis.svgEl);
 
+        this.createScale(body);
     }
-    createPoints(points: number[])
+    private createScale(body: SVGElement)
     {
-        let newPoints = "";
-        points.forEach(el =>
+        for (let i = 1; i <= 24; i++)
         {
-            newPoints += el + " ";
-        });
-        return newPoints
+            let x = this.axis.x + this.oneHour * i;
+            const y = this.axis.y + this.axis.height;
+            const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+            line.setAttribute("x1", `${x}`);
+            line.setAttribute("y1", `${y - this.scale.hours.height / 3}`);
+            line.setAttribute("x2", `${x}`);
+            line.setAttribute("y2", `${y + this.scale.hours.height / 2}`);
+            line.setAttribute("stroke", `${this.axis.color}`);
+            line.setAttribute("fill", `transparent`);
+            line.setAttribute("stroke-width", `${this.axis.sWidth}`);
+
+            const number = document.createElementNS("http://www.w3.org/2000/svg", "text");
+            number.setAttribute("x", `${x - 6}`);
+            number.setAttribute("y", `${y + this.scale.hours.fontSize + this.scale.hours.height / 2}`);
+            number.setAttribute("font-size", `${this.scale.hours.fontSize}`);
+            number.setAttribute("fill", `${this.scale.hours.color}`);
+            number.innerHTML = `${i}`;
+
+            this.scale.hours.els[i] = { line, number };
+            body.appendChild(this.scale.hours.els[i].line);
+            body.appendChild(this.scale.hours.els[i].number);
+
+            x = this.axis.x + this.oneHour * (i - 1);
+            for (let o = 1; o < 60; o++)
+            {
+                const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+                line.setAttribute("x1", `${x + this.oneHour / 60 * o}`);
+                line.setAttribute("y1", `${y - this.scale.minutes.height / 3}`);
+                line.setAttribute("x2", `${x + this.oneHour / 60 * o}`);
+                line.setAttribute("y2", `${y + this.scale.minutes.height / 2}`);
+                line.setAttribute("stroke", `${this.axis.color}`);
+                line.setAttribute("fill", `transparent`);
+                line.setAttribute("stroke-width", `${this.axis.sWidth}`);
+
+                const number = document.createElementNS("http://www.w3.org/2000/svg", "text");
+                number.setAttribute("x", `${x + this.oneHour / 60 * o - 6}`);
+                number.setAttribute("y", `${y + this.scale.minutes.fontSize + this.scale.minutes.height / 2}`);
+                number.setAttribute("font-size", `${this.scale.minutes.fontSize}`);
+                number.setAttribute("fill", `${this.scale.minutes.color}`);
+                number.innerHTML = `${o}`;
+
+                this.scale.minutes.els[o + i*60] = { line, number };
+                body.appendChild(this.scale.minutes.els[o + i*60].line);
+                body.appendChild(this.scale.minutes.els[o + i*60].number);
+            }
+        }
+    }
+    private createAxisPoints()
+    {
+        return `${this.axis.x} ${this.axis.y}
+                ${this.axis.x} ${this.axis.y + this.axis.height}
+                ${this.axis.x + this.axis.width} ${this.axis.y + this.axis.height}`
+    }
+
+    public zoomIt(zoom: number, newWidth: number)
+    {
+        this.width = newWidth;
+        this.axis.width = this.width;
+
+        this.axis.svgEl.setAttribute("points", this.createAxisPoints());
+
+        for (let i = 1; i <= 24; i++)
+        {
+            let x = this.axis.x + this.oneHour * i * zoom;
+            const line = this.scale.hours.els[i].line;
+            line.setAttribute("x1", `${x}`);
+            line.setAttribute("x2", `${x}`);
+
+            const number = this.scale.hours.els[i].number;
+            number.setAttribute("x", `${x - 6}`);
+
+            x = this.axis.x + this.oneHour * (i - 1) * zoom;
+            for (let o = 1; o < 60; o++)
+            {
+                const line = this.scale.minutes.els[o + i*60].line;
+                line.setAttribute("x1", `${x + this.oneHour / 60 * o * zoom}`);
+                line.setAttribute("x2", `${x + this.oneHour / 60 * o * zoom}`);
+                const number = this.scale.minutes.els[o + i*60].number;
+                number.setAttribute("x", `${x + this.oneHour / 60 * o * zoom - 6}`);
+            }
+        }
     }
 }
--- ts/scheduleViewer.ts
+++ ts/scheduleViewer.ts
@@ -2,41 +2,50 @@ import { Coordinates } from "./objects/coordinates.js";
 
 export class scheduleViewer
 {
-    private className_main: string;
-    private className_grafic: string;
-
     private svgBody: SVGSVGElement;
-    private body_width: number;
-    private body_height: number;
 
-    private graficPrm = { x: 0, y: 0, width: 0, height: 0 };
-    private graficBody = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    private parametrs = { x: 50, y: 50, width: 0, height: 0 };
+    private body = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
     private coordinates: Coordinates;
-    private graficMap = new Map<EventTarget, any>();
+    private ctrlPressed = false;
+    private oneHour = 50;
+    private zoom = 1;
+    private zoomMin = 0.6;
+    private zoomMax = 40;
+    private zoomSpeed = 2;
 
     constructor(svg: SVGSVGElement)
     {
         this.svgBody = svg;
-        this.className_main = this.svgBody.id + "scheduleViewer";
-        this.className_grafic = this.className_main + "grafic";
-
+        this.svgBody.setAttribute("width", `${this.oneHour * 25 + this.parametrs.x + 50}`);
         const scgBCR = this.svgBody.getBoundingClientRect()
-        this.body_width = scgBCR.width;
-        this.body_height = scgBCR.height;
-        this.svgBody.setAttribute("transform", "scale(1 -1)");
 
-        this.graficPrm.x = 50;
-        this.graficPrm.y = 50;
-        this.graficPrm.width = scgBCR.width - 100;
-        this.graficPrm.height = scgBCR.height - 100;
+        this.parametrs.width = this.oneHour * 25;
+        this.parametrs.height = scgBCR.height - this.parametrs.y - 50;
 
         {
-            this.svgBody.appendChild(this.graficBody);
-            this.coordinates = new Coordinates(this.graficBody, this.graficPrm, this.graficMap);
+            this.svgBody.appendChild(this.body);
+            this.coordinates = new Coordinates(this.body, this.parametrs, this.oneHour);
         }
-        this.graficBody.addEventListener("click", (e) =>
-        {
-            if (e.target != null) console.log(this.graficMap.get(e.target));
-        })
+        this.svgBody.addEventListener("wheel", (e) => { if (this.ctrlPressed) this.mouseWheel(e) });
+        document.addEventListener("keydown", (e) => {if (e.key == "Control") this.ctrlPressed = true;});
+        document.addEventListener("keyup", (e) =>   {if (e.key == "Control") this.ctrlPressed = false;});
+    }
+
+    mouseWheel(e: WheelEvent)
+    {
+        e.preventDefault();
+        const dy = e.deltaY;
+        const dz = (dy / Math.abs(dy)) * -1;
+
+        this.zoom = Math.max(Math.min(this.zoom + dz * this.zoomSpeed, this.zoomMax), this.zoomMin);
+        this.zoom = Math.round(this.zoom * 100) / 100;
+        console.log(this.zoom);
+
+        const newWidth = this.oneHour * 25 * this.zoom + this.parametrs.x + 50;
+        this.svgBody.setAttribute("width", `${newWidth}`);
+        this.parametrs.width = this.oneHour * 25 * this.zoom;
+        this.coordinates.zoomIt(this.zoom, this.parametrs.width);
     }
 }
